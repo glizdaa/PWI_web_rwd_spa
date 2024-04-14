8713b298589fee9a6f1df0c84725b6659448f10b
glizdaa <glizda95@gmail.com>
2024-04-15 01:38:42 +0200

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link
 import '../globalStyles.css';
 
-function Sidebar({ isOpen }) { // Przyjęcie props isOpen
+function Sidebar({ isOpen }) {
     return (
       <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-        {/* Zawartość Sidebar */}
+        <ul>
+          <li><Link to="/placeholder1">Link Placeholder 1</Link></li>
+          <li><Link to="/placeholder2">Link Placeholder 2</Link></li>
+          <li><Link to="/placeholder3">Link Placeholder 3</Link></li>
+          <li><Link to="/placeholder4">Link Placeholder 4</Link></li>
+          {/* Dodaj więcej linków-placeholderów według potrzeb */}
+        </ul>
       </div>
     );
-  }  
+}  
 
 export default Sidebar;
