684e04586a335feb1ec71ccef780971040d47c4b
glizdaa <glizda95@gmail.com>
2024-04-15 00:40:58 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,28 +2,26 @@ import React, { useState } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-import Sidebar from './components/Sidebar'; // Upewnij się, że Sidebar jest zaimportowany
 import './globalStyles.css';
 
 function App() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false); // Stan dla Sidebar
-
-  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen); // Funkcja do przełączania Sidebar
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
     <Router>
       <div className="App">
-        <Header />
-        <button onClick={toggleSidebar} className="sidebar-toggle">Toggle Sidebar</button> {/* Przycisk do przełączania widoczności Sidebar */}
-        <Sidebar isOpen={isSidebarOpen} /> {/* Przekazanie stanu do Sidebar jako props */}
+        <Header toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
+        <Sidebar isOpen={isSidebarOpen} />
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
+            {/* Możesz dodać więcej ścieżek tutaj */}
           </Routes>
         </main>
         <Footer />
