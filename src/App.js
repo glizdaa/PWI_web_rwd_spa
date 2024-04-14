b2232c91427caed51df6c4aadc1017b7d08afacd
glizdaa <glizda95@gmail.com>
2024-04-15 00:36:49 +0200

    test5

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Sidebar from './components/Sidebar'; // Upewnij się, że Sidebar jest zaimportowany
 import './globalStyles.css';
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false); // Stan dla Sidebar
+
+  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen); // Funkcja do przełączania Sidebar
+
   return (
     <Router>
       <div className="App">
         <Header />
-        <Sidebar /> {/* Dodaj Sidebar tutaj */}
+        <button onClick={toggleSidebar} className="sidebar-toggle">Toggle Sidebar</button> {/* Przycisk do przełączania widoczności Sidebar */}
+        <Sidebar isOpen={isSidebarOpen} /> {/* Przekazanie stanu do Sidebar jako props */}
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,25 +1,12 @@
 import React, { useState } from 'react';
 import '../globalStyles.css';
 
-function Sidebar() {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleSidebar = () => setIsOpen(!isOpen);
-
+function Sidebar({ isOpen }) { // Przyjęcie props isOpen
     return (
-        <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-            <button onClick={toggleSidebar}>Toggle Sidebar</button>
-            <div className="sidebar-content">
-                <h2>Sidebar Title</h2>
-                <ul>
-                    <li>Link 1</li>
-                    <li>Link 2</li>
-                    <li>Link 3</li>
-                    // Dodaj więcej linków lub zawartości, jak potrzebujesz
-                </ul>
-            </div>
-        </div>
+      <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+        {/* Zawartość Sidebar */}
+      </div>
     );
-}
+  }  
 
 export default Sidebar;
