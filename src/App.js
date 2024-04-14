ab122c2463d3ced28b97f16cbf0050f31d28011e
glizdaa <glizda95@gmail.com>
2024-04-15 01:13:14 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -6,19 +6,22 @@ import Sidebar from './components/Sidebar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-import './globalStyles.css'; // Upewnij się, że tutaj importujesz swoje style CSS
+import './globalStyles.css';
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <Router>
       <div className="App">
-        <Header />
+        <Header toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
+        <Sidebar isOpen={isSidebarOpen} />
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
-            {/* Tutaj możesz dodać więcej ścieżek, jeśli są potrzebne */}
+            {/* Możesz dodać więcej ścieżek tutaj */}
           </Routes>
         </main>
         <Footer />
