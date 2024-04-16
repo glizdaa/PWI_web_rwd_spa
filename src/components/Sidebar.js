bf87786470d2e15b7f7ce808b46286347c3bf9fd
glizdaa <glizda95@gmail.com>
2024-04-17 01:45:27 +0200

    t

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,7 +6,7 @@ import '../globalStyles.css';
 function Sidebar({ isOpen }) {
     return (
       <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-        <><img src={smurfImage} alt="Whoops" style={{ width: '100%', height: 'auto'}} /></>
+        <img src={smurfImage} alt="Whoops" style={{ width: '100%', height: 'auto'}} />
         <ul>
           <li><Link to="/placeholder">Link Placeholder 1</Link></li>
           <li><Link to="/placeholder">Link Placeholder 2</Link></li>
