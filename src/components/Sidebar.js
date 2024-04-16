e9d4aa962b47fa6a2ead04ba1a6a28615ab4c50e
glizdaa <glizda95@gmail.com>
2024-04-17 00:43:38 +0200

    test

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import smurfImage from '../smurf.jpg';
 import '../globalStyles.css';
 
 function Sidebar({ isOpen }) {
     return (
       <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-        O kurde, znalazłeś mnie!
+        <><img src={smurfImage} alt="Whoops" style={{ width: '100%', height: 'auto'}} /></>
         <ul>
           <li><Link to="/placeholder">Link Placeholder 1</Link></li>
           <li><Link to="/placeholder">Link Placeholder 2</Link></li>
